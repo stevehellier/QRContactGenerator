1d0e357488390bde191e0ac3b8f98a3b73ecdeaa
Steve Hellier <steve_hellier@hotmail.com>
2021-09-28 12:33:38 +0100

    Refactor
    * Moved QR Generation functions to it's own class

--- QRContactGenerator/Program.cs
+++ QRContactGenerator/Program.cs
@@ -1,10 +1,5 @@
-﻿using QRCoder;
-using QRContactGenerator.Models;
-using System;
-using System.Drawing;
-using System.Drawing.Imaging;
+﻿using System;
 using System.IO;
-using System.Text;
 
 namespace QRContactGenerator
 {
@@ -36,7 +31,6 @@ namespace QRContactGenerator
 
             try
             {
-
                 var people = Data.LoadPeople(filename);
 
                 if (people != null)
@@ -51,7 +45,7 @@ namespace QRContactGenerator
                     {
                         var personInfo = $"{person.Firstname} {person.Lastname} ({person.Organisation})";
                         Console.Write($"Creating QR vCard for {personInfo}... ");
-                        SaveQRCodeImage(CreateQRCode(person), path, personInfo);
+                        QRCoder.SaveQRCodeImage(QRCoder.CreateQRCode(person), path, personInfo);
                         Console.WriteLine("done!");
                     }
                 }
@@ -66,38 +60,5 @@ namespace QRContactGenerator
         {
             Console.WriteLine("Quitting...");
         }
-
-        private static Image CreateQRCode(PersonModel person, int pixels = 20)
-        {
-            StringBuilder payload = new();
-            payload.AppendLine($"BEGIN:VCARD");
-            payload.AppendLine($"VERSION:3.0");
-            payload.AppendLine($"N:{person.Lastname};{person.Firstname};;{person.Title};");
-            payload.AppendLine($"FN:{person.Title} {person.Firstname} {person.Lastname}");
-            payload.AppendLine($"ORG:{person.Organisation}");
-            payload.AppendLine($"TITLE:{person.JobTitle}");
-            payload.AppendLine($"TEL;type=WORK;type=CELL;type=VOICE:{person.MobilePhone}");
-            payload.AppendLine($"TEL;type=WORK;type=PREF;type=VOICE:{person.WorkPhone}");
-            payload.AppendLine($"ADR;type=WORK:;;{person.HouseNumber} {person.Street};{person.City};{person.County};{person.PostCode};{person.Country}");
-            payload.AppendLine($"URL:{person.Website}");
-            payload.AppendLine($"EMAIL;type=WORK:{person.Email}");
-            payload.AppendLine($"REV:{DateTime.Now:s}");
-            payload.AppendLine($"END:VCARD");
-
-            QRCodeGenerator qrCodeGenerator = new();
-            QRCodeData qrCodeData = qrCodeGenerator.CreateQrCode(payload.ToString(), QRCodeGenerator.ECCLevel.Q);
-            QR qrCode = new(qrCodeData);
-
-            var _qrCodeImage = qrCode.GetGraphic(pixels);
-
-            return _qrCodeImage;
-        }
-        private static void SaveQRCodeImage(Image image, string path, string filename)
-        {
-            filename = Path.Combine(path, filename);
-            image.Save($"{filename}.png", ImageFormat.Png);
-        }
-
-
     }
 }
---
+++ QRContactGenerator/QRCoder.cs
@@ -0,0 +1,44 @@
+﻿using QRCoder;
+using QRContactGenerator.Models;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+
+namespace QRContactGenerator
+{
+    class QRCoder
+    {
+        public static Image CreateQRCode(PersonModel person, int pixels = 20)
+        {
+            StringBuilder payload = new();
+            payload.AppendLine($"BEGIN:VCARD");
+            payload.AppendLine($"VERSION:3.0");
+            payload.AppendLine($"N:{person.Lastname};{person.Firstname};;{person.Title};");
+            payload.AppendLine($"FN:{person.Title} {person.Firstname} {person.Lastname}");
+            payload.AppendLine($"ORG:{person.Organisation}");
+            payload.AppendLine($"TITLE:{person.JobTitle}");
+            payload.AppendLine($"TEL;type=WORK;type=CELL;type=VOICE:{person.MobilePhone}");
+            payload.AppendLine($"TEL;type=WORK;type=PREF;type=VOICE:{person.WorkPhone}");
+            payload.AppendLine($"ADR;type=WORK:;;{person.HouseNumber} {person.Street};{person.City};{person.County};{person.PostCode};{person.Country}");
+            payload.AppendLine($"URL:{person.Website}");
+            payload.AppendLine($"EMAIL;type=WORK:{person.Email}");
+            payload.AppendLine($"REV:{DateTime.Now:s}");
+            payload.AppendLine($"END:VCARD");
+
+            QRCodeGenerator qrCodeGenerator = new();
+            QRCodeData qrCodeData = qrCodeGenerator.CreateQrCode(payload.ToString(), QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new(qrCodeData);
+
+            var _qrCodeImage = qrCode.GetGraphic(pixels);
+
+            return _qrCodeImage;
+        }
+        public static void SaveQRCodeImage(Image image, string path, string filename)
+        {
+            filename = Path.Combine(path, filename);
+            image.Save($"{filename}.png", ImageFormat.Png);
+        }
+    }
+}
